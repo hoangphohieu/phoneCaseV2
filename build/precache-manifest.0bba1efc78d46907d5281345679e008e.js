d545143280b563e430ca89a07e5dc116a8aee467
Hoangphohieu <hoangphohieu1@gmail.com>
2019-09-04 10:24:30 +0700

    hieu 2

---
+++ build/precache-manifest.0bba1efc78d46907d5281345679e008e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ee7c628793d3838088918d4b1f877c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8ad3d5fb6bf817a4a4a",
+    "url": "/static/css/main.6e75493a.chunk.css"
+  },
+  {
+    "revision": "eec7eebe5b5a8374ead9",
+    "url": "/static/js/2.523280a3.chunk.js"
+  },
+  {
+    "revision": "b8ad3d5fb6bf817a4a4a",
+    "url": "/static/js/main.bfd5f7fb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
