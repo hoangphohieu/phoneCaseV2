92047d13475cb358888608e506d5fea55c8a2072
hoangphohieu <hoangphohieu1@gmail.com>
2019-10-29 17:43:49 +0700

    hieu dz

---
+++ app flash/precache-manifest.1eed0d550588618f040023d736f77447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5451827d92d0345cf19f3afa26d1248",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcfadbb1dd2696e7d9cd",
+    "url": "/static/css/main.6e75493a.chunk.css"
+  },
+  {
+    "revision": "eec7eebe5b5a8374ead9",
+    "url": "/static/js/2.523280a3.chunk.js"
+  },
+  {
+    "revision": "bcfadbb1dd2696e7d9cd",
+    "url": "/static/js/main.38a65578.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
