c7d7b7441d77b8f241ca7f27ffbdfbabfb66bb8b
Hoangphohieu <hoangphohieu1@gmail.com>
2019-12-24 23:35:42 +0700

    hieu

---
+++ only DEV/phoneCaseV3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import InputExcel from './components/InputExcel';
+import AboutMe from './components/AboutMe';
+function App() {
+  
+  return (
+    <React.Fragment>
+      <InputExcel/>
+      <AboutMe/>
+    </React.Fragment>
+  );
+}
+
+export default App;
